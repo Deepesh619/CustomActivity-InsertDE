a4bf2379d016d00faeeb940319364396f767eddb
Deepesh619 <d.pralhad.dhande@accenture.com>
2021-05-11 21:18:07 +0530

    cc

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 // -------------------
 var express     = require('express');
 var bodyParser  = require('body-parser');
+//var errorhandler = require('errorhandler');
 var http        = require('http');
 var path        = require('path');
-var routes      = require('./routes');
+//var request     = require('request');
+//var routes      = require('./routes');
 var activity    = require('./routes/activity');
 
 var app = express();
@@ -13,7 +15,10 @@ var app = express();
 // Configure Express
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.raw({type: 'application/jwt'}));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
+//app.use(express.methodOverride());
+//app.use(express.favicon());
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,12 +30,16 @@ if ('development' == app.get('env')) {
 
 }
 
+// HubExchange Routes
+//app.get('/', routes.index );
+//app.post('/login', routes.login );
+//app.post('/logout', routes.logout );
 
 // Custom Hello World Activity Routes
-app.post('/journeybuilder/save', activity.save );
-app.post('/journeybuilder/validate', activity.validate );
-app.post('/journeybuilder/publish', activity.publish );
-app.post('/journeybuilder/execute', activity.execute );
+app.post('/journeybuilder/save/', activity.save );
+app.post('/journeybuilder/validate/', activity.validate );
+app.post('/journeybuilder/publish/', activity.publish );
+app.post('/journeybuilder/execute/', activity.execute );
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
